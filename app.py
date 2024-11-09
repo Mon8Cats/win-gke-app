002f7157b7defa80bcdbed36c27f18a2b3d9f3f3
SteveKimMHS <Steve.Kim@MHShealth.com>
2024-11-09 16:40:22 -0600

    turn of debug

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def default_error_handler(error):
     return {"message": str(error)}, getattr(error, "code", 500)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
 
 
 #

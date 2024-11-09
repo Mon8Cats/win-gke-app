bbf4c6f349cf3b42746fa07516a8638766716304
SteveKimMHS <Steve.Kim@MHShealth.com>
2024-11-09 17:25:23 -0600

    test

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.config["API_TITLE"] = "User API"
 app.config["API_VERSION"] = "1.0"
 app.config["OPENAPI_VERSION"] = "3.0.2"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger"
+app.config["OPENAPI_URL_PREFIX"] = "/docs"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
 app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
 api = Api(app)
